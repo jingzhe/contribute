16eeb8a5e150026fdbdc2048aa9842366e367fa6
Jingzhe Yu <jingzhe@gmail.com>
2020-03-29 13:28:30 +0300

    Try thl data deployment

--- src/app/service/thl.service.ts
+++ src/app/service/thl.service.ts
@@ -10,7 +10,7 @@ import { ThlDistrictData } from './patientdata';
 
 export class ThlService {
 
-  endpoint = '/pivot/prod/en/epirapo/covid19case/fact_epirapo_covid19case.json';
+  endpoint = 'https://sampo.thl.fi/pivot/prod/en/epirapo/covid19case/fact_epirapo_covid19case.dimensions.json';
   districtDataArray: ThlDistrictData[] = [];
   districtNameMap = {};
 
