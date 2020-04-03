9c278a3ce0f918ad22ee92938f60c314d6f2a85b
Jingzhe Yu <jingzhe@gmail.com>
2020-04-03 12:14:21 +0300

    Show yesterday's new number

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -74,6 +74,7 @@ export class DataService {
 
   calculateTodayInfo() {
     let today = new Date();
+    today.setDate(today.getDate() - 1);
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     let yyyy = today.getFullYear();
